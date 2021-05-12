95900af8a7eca072ec4d025d7a8a142cec54d728
Danila-Bukin <60630969+Danila-Bukin@users.noreply.github.com>
2021-05-12 20:33:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,51 +7,43 @@ export default (express, bodyParser, createReadStream, writeFileSync, crypto, ht
     const url = 'mongodb+srv://danilabukin:danilabukin>@cluster0.wcetp.mongodb.net/week7demo?retryWrites=true&w=majority'
 
     app
-    .use((r, res, next) => r.res.set({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE"}) && next())
-    .use(bodyParser.urlencoded({ extended: true }))
-    .all('/req/', (req, res) => {
-        const addr = req.method === 'POST' ? req.body.addr : req.query.addr;
-
-        http.get(addr, (r, b = '') => {
-            r
-            .on('data', d => b += d)
-            .on('end', () => res.send(b));
-        });
+    .use((req, res, next) => {
+      res.append('Access-Control-Allow-Origin', ['*']);
+      res.append('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
+      res.append('Access-Control-Allow-Headers', '*');
+      next();
     })
-    .get('/login/', (req, res) => res.send('bee_joo'))
-    .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
-    .get('/sha1/:input/', (req, res) => res.send(crypto.createHash('sha1').update(req.params.input).digest('hex')))
-    .post('/insert/', urlencodedParser, async (req, res) => {
-        const log = req.body.login;
-        const pass = req.body.password;
-        const url = req.body.URL;
 
-        await m.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    .use(bodyParser.json())
 
-        const newUser = new User({login: log, password: pass});
-        try {
-            await newUser.save();
-            res.status(201);
-        } catch (e) {
-            res.status(400);
-        }
-        res.end();
+    .all('/login/', (req, res) => {
+        res.end("bee_joo");
     })
-    .get('/test/', urlencodedParser, async (req, res) => {
 
-        const browser = await puppeteer.launch({args: ['--no-sandbox']});
-        const page = await browser.newPage();
-        await page.goto(req.query.URL);
-        await page.click('#bt');
-        const input = await page.$('#inp');
-        let value = await page.evaluate(inp => inp.value, input);
-        res.send(value);
-
-        res.send(url);
-        
+    .all('/code/', (req, res) => {
+        res.set({ 'Content-Type': 'text/plain; charset=utf-8' });
+        createReadStream(import.meta.url.substring(7)).pipe(res);
     })
+
+    .get('/sha1/:input', r => {
+        const shasum = crypto.createHash('sha1');
+        shasum.update(r.params.input);
+        r.res.send(shasum.digest('hex')); 
+    })
+
+    .get('/req/', (req, res) => {
+        const url = req.query.addr;
+        http.get(String(url), response => {
+            let data = '';
+            response.on('data', chunk => {
+                data += chunk;
+            })
+            response.on('end', () => {
+                res.end(data);
+            })
+        })
+    })
+
     .get('/wordpress/*', (req, res) => {
         console.log('http://f0539855.xsph.ru/wordpress/'+req.params[0]);
         res.header('Content-Type', 'application/json');
@@ -61,16 +53,7 @@ export default (express, bodyParser, createReadStream, writeFileSync, crypto, ht
             .on('end', () => res.send(buffer));
         });
     })
-    .post('/render/', json_parser, urlencodedParser, (req, res) => {
-      let addr = req.query.addr;
-      console.log(addr, req.body.random2, req.body.random3);
-      void http.get(addr, (r, buffer='') => {
-            r
-            .on('data', data => buffer += data)
-            .on('end', () => writeFileSync('views/data.pug', buffer));
-        });
-        res.render('data.pug', {'random2': req.body.random2, 'random3': req.body.random3});
-    })
+
     .get('/test/', urlencodedParser, async (req, res) => {
 
         const browser = await puppeteer.launch({args: [
@@ -93,7 +76,51 @@ export default (express, bodyParser, createReadStream, writeFileSync, crypto, ht
         res.send(url);
         
     })
-    .all('/*', r => r.res.send('bee_joo'));
+
+    .post('/req/', urlencodedParser, (req, res) => {
+        const url = req.body.addr;
+        http.get(String(url), response => {
+            let data = '';
+            response.on('data', chunk => {
+                data += chunk;
+            })
+            response.on('end', () => {
+                res.end(data);
+            })
+        })
+    })
+
+    .post('/insert/', urlencodedParser, async (req, res) => {
+        const log = req.body.login;
+        const pass = req.body.password;
+        const url = req.body.URL;
+
+        await m.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        const newUser = new User({login: log, password: pass});
+        try {
+            await newUser.save();
+            res.status(201);
+        } catch (e) {
+            res.status(400);
+        }
+        res.end();
+    })
+
+    .post('/render/', json_parser, urlencodedParser, (req, res) => {
+      let addr = req.query.addr;
+      console.log(addr, req.body.random2, req.body.random3);
+      void http.get(addr, (r, buffer='') => {
+            r
+            .on('data', data => buffer += data)
+            .on('end', () => writeFileSync('views/data.pug', buffer));
+        });
+        res.render('data.pug', {'random2': req.body.random2, 'random3': req.body.random3});
+    })
+
+    .all('/*', (req, res) => {
+        res.end("bee_joo");
+    })
 
     return app;
 }
